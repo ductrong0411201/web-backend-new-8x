b3e88c10641d7752d1da09bdd56b56503f8206a9
NVTuann201 <nvtuann201@gmail.com>
2023-02-03 15:35:19 +0700

    update construction controller

--- app/Models/Asset/Construction.php
+++ app/Models/Asset/Construction.php
@@ -5,6 +5,7 @@ namespace App\Models\Asset;
 use App\Helpers\StringHelper;
 use App\Models\Access\User\User;
 use Illuminate\Database\Eloquent\Model;
+
 /**
  * Class User.
  */
@@ -18,30 +19,37 @@ class Construction extends Model
     protected $table = 'constructions';
     protected $appends = ["display_name"];
 
-    public function orders () {
+    public function orders()
+    {
         return $this->hasMany(Order::class);
     }
 
-    public function order () {
+    public function order()
+    {
         return $this->hasOne(Order::class);
     }
 
-    public function area () {
+    public function area()
+    {
         return $this->belongsTo(Area::class, 'area_id', 'gid');
     }
 
-    public function user () {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function department () {
+    public function department()
+    {
         return $this->belongsTo(Department::class);
     }
 
-    public function structure () {
+    public function structure()
+    {
         return $this->belongsTo(Structure::class);
     }
-    public function funding_agency () {
+    public function funding_agency()
+    {
         return $this->belongsTo(FundingAgency::class);
     }
 
@@ -50,9 +58,10 @@ class Construction extends Model
      *
      * @var array
      */
-    protected $fillable = ['user_id', 'name', 'latitude', 'longitude', 'structure_id', 'department_id', 'area_id', 'funding_agency_id', 'meta'];
+    protected $fillable = ['user_id', 'name', 'latitude', 'longitude', 'structure_id', 'department_id', 'area_id', 'funding_agency_id', 'meta', 'geom'];
 
-    public function getDisplayNameAttribute() {
+    public function getDisplayNameAttribute()
+    {
         return StringHelper::convertFromSmallCaps($this->name);
     }
 }
